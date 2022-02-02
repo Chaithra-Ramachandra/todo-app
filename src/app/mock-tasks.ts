93dde78d534f800b4be123e0a629522e8a6a0a53
chaithraramachandraa <76424501+chaithraramachandraa@users.noreply.github.com>
2022-02-02 21:19:37 +0530

    todo-app using Angular 11

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,26 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+
+{
+    id:1,
+    text: 'Doctor Appointment',
+    day:'Feb 1st at 1:30pm',
+    reminder: true,
+},
+
+{
+    id:2,
+    text: 'Dietician Appointment',
+    day:'March 2nd at 1:30pm',
+    reminder: true,
+},
+
+{
+    id:3,
+    text: 'Physician Appointment',
+    day:'Feb 1st at 1:30pm',
+    reminder: false,
+}
+
+]
